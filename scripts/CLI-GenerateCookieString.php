57909c3bd365e8dae7f3d5e95fff5b47c661d40b
Shawn M. Jones <smj@littleprojects.org>
2013-08-16 11:25:05 -0400

    removed unnecessary variables from CLI script

--- scripts/CLI-GenerateCookieString.php
+++ scripts/CLI-GenerateCookieString.php
@@ -3,7 +3,5 @@
 require_once(__DIR__ . '/../tests/lib/HTTPFetch.php');
 
 $sessionString = authenticateWithMediawiki();
-$uagent = 'Memento Tester';
-$ACCEPTDATETIME = 'Thu, 01 Jan 1970 00:00:00 GMT';
 
 echo $sessionString;
