abc0f0de7065e27fd40658103694772bb0b3297c
Shawn M. Jones <smj@littleprojects.org>
2013-10-25 17:06:19 -0400

    raised version to 2.0-beta1

--- Memento/Memento.php
+++ Memento/Memento.php
@@ -43,7 +43,7 @@ $wgExtensionCredits['specialpage'][] = array(
 		'Robert Sanderson',
 		'Shawn M. Jones'
 		),
-	'version' => '2.0-alpha4-SNAPSHOT'
+	'version' => '2.0-beta1'
 );
 
 // Set up the messages file
