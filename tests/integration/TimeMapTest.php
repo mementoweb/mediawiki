150ad25c6a58919f87cd66184af45373f27c130d
Shawn M. Jones <jones.shawn.m@gmail.com>
2019-05-20 13:55:57 -0600

    integration tests for increasing and decreasing timemaps

--- tests/integration/TimeMapTest.php
+++ tests/integration/TimeMapTest.php
@@ -61,4 +61,74 @@ class TimeMapTest extends PHPUnit\Framework\TestCase {
 		$this->assertEquals( $entity, $expected_timemap_data );
 
 	}
+
+	/**
+	 * @group timemap
+	 *
+	 */
+	public function testSimpleIncreasingTimeMap() {
+
+		global $sessionCookieString;
+
+		$uagent = "Memento-Mediawiki-Plugin/Test";
+
+		$expected_timemap_file = getenv( 'TESTDATADIR' ) . '/expected_increasing_timemap.link';
+
+		$myfile = fopen($expected_timemap_file, "r") or die("cannot open $expected_timemap_file");
+		$expected_timemap_data = fread($myfile, filesize($expected_timemap_file));
+		fclose($myfile);
+
+		$urit = "http://localhost:8080/index.php/Special:TimeMap/20130522211900/1/Kevan_Lannister";
+
+		$curlCmd = "curl -v -s -A '$uagent' -b '$sessionCookieString' -k -i --url \"$urit\"";
+
+		$outputfile = __CLASS__ . '.' . __FUNCTION__ . '.' . self::$instance . '.txt';
+		$debugfile = __CLASS__ . '.' . __FUNCTION__ . '-debug-' . self::$instance . '.txt';
+
+		$response = `$curlCmd 2> $debugfile | tee -a "$outputfile"`;
+
+		$headers = extractHeadersFromResponse( $response );
+		$statusline = extractStatuslineFromResponse( $response );
+		$entity = extractEntityFromResponse( $response );
+
+		$this->assertEquals( $statusline["code"], "200" );
+
+		$this->assertEquals( $entity, $expected_timemap_data );
+
+	}
+
+	/**
+	 * @group timemap
+	 *
+	 */
+	public function testSimpleDecreasingTimeMap() {
+
+		global $sessionCookieString;
+
+		$uagent = "Memento-Mediawiki-Plugin/Test";
+
+		$expected_timemap_file = getenv( 'TESTDATADIR' ) . '/expected_decreasing_timemap.link';
+
+		$myfile = fopen($expected_timemap_file, "r") or die("cannot open $expected_timemap_file");
+		$expected_timemap_data = fread($myfile, filesize($expected_timemap_file));
+		fclose($myfile);
+
+		$urit = "http://localhost:8080/index.php/Special:TimeMap/20130522211900/-1/Kevan_Lannister";
+
+		$curlCmd = "curl -v -s -A '$uagent' -b '$sessionCookieString' -k -i --url \"$urit\"";
+
+		$outputfile = __CLASS__ . '.' . __FUNCTION__ . '.' . self::$instance . '.txt';
+		$debugfile = __CLASS__ . '.' . __FUNCTION__ . '-debug-' . self::$instance . '.txt';
+
+		$response = `$curlCmd 2> $debugfile | tee -a "$outputfile"`;
+
+		$headers = extractHeadersFromResponse( $response );
+		$statusline = extractStatuslineFromResponse( $response );
+		$entity = extractEntityFromResponse( $response );
+
+		$this->assertEquals( $statusline["code"], "200" );
+
+		$this->assertEquals( $entity, $expected_timemap_data );
+
+	}
 }
